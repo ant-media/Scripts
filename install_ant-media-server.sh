87dcaed0fb720860378efd57745993f5c3799eec
Ahmet Oğuz Mermerkaya <ahmetmermerkaya@gmail.com>
2018-12-06 12:52:53 +0300

    Set default value for SAVE_SETTINGS field

--- install_ant-media-server.sh
+++ install_ant-media-server.sh
@@ -8,7 +8,15 @@
 
 AMS_BASE=/usr/local/antmedia
 BACKUP_DIR="/usr/local/antmedia-backup-"$(date +"%Y-%m-%d_%H-%M-%S")
-SAVE_SETTINGS=$2
+SAVE_SETTINGS=false
+
+if [ ! -z "$2" ]; then
+  echo "Please give the Ant Media Server zip file as parameter"
+  echo "$0  ant-media-server-....zip"
+  exit 1
+fi
+
+
 
 restore_settings() {
   #app settings
@@ -47,6 +55,10 @@ if [ -z "$1" ]; then
   echo "Please give the Ant Media Server zip file as parameter"
   echo "$0  ant-media-server-....zip"
   exit 1
+fi
+
+if [ ! -z "$2" ]; then
+   SAVE_SETTINGS=$2
 fi
 
 SUDO="sudo"
