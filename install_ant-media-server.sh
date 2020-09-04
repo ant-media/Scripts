eb648fc611fed411328e48166a77335fc0292ad1
Can Arikan <arikan.canates@googlemail.com>
2020-09-04 18:24:04 +0300

    export changed java home

--- install_ant-media-server.sh
+++ install_ant-media-server.sh
@@ -148,6 +148,7 @@ $SUDO sed -i '/JAVA_HOME="\/usr\/lib\/jvm\/java-11-oracle"/c\JAVA_HOME="\/usr\/l
 check
 echo "export JAVA_HOME=\/usr\/lib\/jvm\/java-11-openjdk-amd64" >>~/.bashrc
 source ~/.bashrc
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 echo "JAVA_HOME : $JAVA_HOME"
 
 $SUDO cp $AMS_BASE/antmedia.service /lib/systemd/system/
