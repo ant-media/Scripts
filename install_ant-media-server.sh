050194ec33a2784fed188c14c7a52a335ad5d73b
muratugureminoglu <murat@murat.ws>
2020-11-02 21:01:54 +0300

    fix description

--- install_ant-media-server.sh
+++ install_ant-media-server.sh
@@ -89,7 +89,7 @@ distro () {
   os_release="/etc/os-release"
   if [ -f "$os_release" ]; then
     . $os_release
-    msg="We are supporting Ubuntu 16.04, 18.04 and Centos 7."
+    msg="We are supporting Ubuntu 18.04, 20.04 and Centos 7."
     if [ "$ID" == "ubuntu" ] || [ "$ID" == "centos" ]; then  
       if [ "$VERSION_ID" != "18.04" ] && [ "$VERSION_ID" != "20.04" ] && [ "$VERSION_ID" != "7" ] ; then
          echo $msg
