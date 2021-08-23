d01bcd130528dd7bc8b98cdef7c5dc671cecdc0d
mekya <ahmetmermerkaya@gmail.com>
2021-08-23 14:39:15 +0300

    Create change log based on PR and MR

--- prepare_changelog.sh
+++ prepare_changelog.sh
@@ -4,7 +4,6 @@
 # Prepare change log clones the following repositories and creates change log since latest tag
 # "Ant-Media-Server"
 # "Ant-Media-Enterprise"
-# "Ant-Media-Server-Common"
 # "ManagementConsole_AngularApp"
 # "StreamApp"
 # "Ant-Media-Server-Parent"
@@ -15,6 +14,11 @@
 # BRANCH_NAME is the branch where change log will be created. It's optional.
 # TAG is the tag where change log will be created since then. It's optional. If not specified, it will create the changelog
 # since the last tag
+#
+# Important: To get MR from gitlab, there should be environment variable(GITLAB_TOKEN) exported with valid token
+# To get PR from github, there should be environment variable(GITHUB_TOKEN) exported with valid token
+# 
+
 
 
 BRANCH_NAME=$1
@@ -27,23 +31,25 @@ get_change_log()
 	FOLDER=$2
 	
 	if [ $(git ls-remote $GIT_URL $BRANCH_NAME  | wc -l) == "1" ];  
-    then 
+  then 
       echo " $BRANCH_NAME branch found";  
       git clone --depth=200 -b $BRANCH_NAME $GIT_URL $FOLDER;  
         
-    else
+  else
       echo "branch not found. Checking out master"; 
       git clone --depth=200 $GIT_URL $FOLDER;  
-    fi
+  fi
 
-    cd $FOLDER
+  cd $FOLDER
 	if [ ! -n "$TAG" ]; then
 	  echo "TAG parameter not supplied.";
 	  TAG=`git tag --sort=-creatordate | head -n 1`;
 	fi
 
+  DATE_OF_THE_TAG=`git log -1 --date=iso-strict --pretty=format:%ad $TAG`
+
   echo "<h3>$FOLDER</h3>" >> $CHANGE_LOG
-  git log --no-merges --pretty=format:"<li> <a href='http://github.com/ant-media/$FOLDER/commit/%H'>%h</a> %s - %ci</li>" --reverse $TAG..HEAD >> $CHANGE_LOG
+  gh pr list --state merged --search "created:>$DATE_OF_THE_TAG" -L 1000 --json title,body,number --template '{{range .}} <li> <a href="http://github.com/ant-media/Ant-Media-Server/pull/{{.number}}">{{.number}}</a> {{.title}} - <a href="https://github.com/ant-media/Ant-Media-Server/issues/{{.body}}">{{.body}}</a></li>{{end}}' >> $CHANGE_LOG
 
   cd ..
  
@@ -59,6 +65,8 @@ li , body {
 </style>" >> $CHANGE_LOG
 
 #get Ant Media Server
+export GH_REPO=ant-media/Ant-Media-Server
+export GH_HOST=github.com
 get_change_log https://github.com/ant-media/Ant-Media-Server.git Ant-Media-Server
 
 #get Ant Media Server Enterprise log
@@ -67,16 +75,18 @@ if [ ! -n "$TAG" ]; then
   TAG=`git tag --sort=-creatordate | head -n 1`;
 fi
 echo "<h3>Ant-Media-Enterprise</h3>" >> $CHANGE_LOG
-git log --no-merges --pretty=format:"<li>%h - %s - %ci</li>"  $TAG..HEAD --reverse >> $CHANGE_LOG
 
-# "Ant-Media-Server-Common"
-get_change_log https://github.com/ant-media/Ant-Media-Server-Common.git Ant-Media-Server-Common
+DATE_OF_THE_TAG=`git log -1 --date=iso-strict --pretty=format:%ad $TAG`
+curl --location --request GET "https://gitlab.com/api/v4/projects/5032874/merge_requests?state=merged&view=simple&per_page=100&created_after=$DATE_OF_THE_TAG" --header "Authorization: Bearer $GITLAB_TOKEN" 2>/dev/null  | jq '"<li>" + .[].title + "- <a href=https://github.com/ant-media/Ant-Media-Server/issues/" + .[].description + ">" + .[].description + "</a></li>"' >> $CHANGE_LOG
 
 # "StreamApp"
+export GH_REPO=ant-media/StreamApp
 get_change_log https://github.com/ant-media/StreamApp.git StreamApp
 
 # "Ant-Media-Server-Parent"
+export GH_REPO=ant-media/ant-media-server-parent
 get_change_log https://github.com/ant-media/ant-media-server-parent.git Ant-Media-Server-Parent
 
 # "ManagementConsole_AngularApp"
+export GH_REPO=ant-media/ManagementConsole_AngularApp
 get_change_log https://github.com/ant-media/ManagementConsole_AngularApp.git ManagementConsole_AngularApp
