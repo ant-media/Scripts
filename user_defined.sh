a3976b90de02cb835137a8c52904b6f7d0819fe8
Murat Ugur Eminoglu <murat@murat.ws>
2021-09-19 12:46:52 +0300

    fix syntax

--- user_defined.sh
+++ user_defined.sh
@@ -32,13 +32,13 @@ chown -R antmedia:antmedia $TARGET
 upload(){
 
 	if [ -z `which aws` ]; then
-	rm -r aws* > /dev/null 2>&1
-	echo "Please wait. AWS Client is installing..."
-	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" > /dev/null 2>&1
-	unzip awscliv2.zip > /dev/null 2>&1 
-	sudo ./aws/install &
-	wait $!
-	rm -r aws*
+		rm -r aws* > /dev/null 2>&1
+		echo "Please wait. AWS Client is installing..."
+		curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" > /dev/null 2>&1
+		unzip awscliv2.zip > /dev/null 2>&1 
+		sudo ./aws/install &
+		wait $!
+		rm -r aws*
 	fi
 
 	# Set credentials 
