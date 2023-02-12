9801ccc782d1c9e3120a186af7e47c1e23f5ffdd
mekya <ahmetmermerkaya@gmail.com>
2023-02-12 19:15:02 +0300

    Add tee command

--- install_ant-media-server.sh
+++ install_ant-media-server.sh
@@ -346,7 +346,7 @@ then
 fi
 
 # create a logrotate config file
-cat << EOF | $SUDO /etc/logrotate.d/antmedia
+cat << EOF | $SUDO tee /etc/logrotate.d/antmedia
 /var/log/antmedia/antmedia-error.log {
     daily
     create 644 antmedia antmedia
