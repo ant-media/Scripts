98cdd3b91b85d644b0aa2a678bd869b28f852862
Murat Ugur Eminoglu <murat@murat.ws>
2023-02-20 14:50:42 +0300

    Fix Java path on rpm-based distro

--- install_ant-media-server.sh
+++ install_ant-media-server.sh
@@ -290,7 +290,10 @@ else
     $SUDO apt-get update -y
     $SUDO apt-get install openjdk-11-jre-headless -y
     check
-  fi
+  elif [ "$ID" == "centos" ] || [ "$ID" == "almalinux" ] || [ "$ID" == "rocky" ]; then
+    $SUDO yum -y install java-11-openjdk-headless
+    ln -s $(readlink -f $(which java) | rev | cut -d "/" -f3- | rev) /usr/lib/jvm/java-11-openjdk-amd64
+  fi 
   echo "export JAVA_HOME=\/usr\/lib\/jvm\/java-11-openjdk-amd64/" >>~/.bashrc
   source ~/.bashrc
   export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/
