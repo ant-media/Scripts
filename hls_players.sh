0973ea6bb1374d5f77d1239cfd5328582638fae2
Mohit-3196 <86982446+Mohit-3196@users.noreply.github.com>
2023-04-12 16:53:46 +0530

    updating hls_players.sh
    Trying to resolve the errors in the script

--- hls_players.sh
+++ hls_players.sh
@@ -3,9 +3,9 @@
 m3u8_url=$1
 viewers=$2
 
-for (( i=1; i <=viewers; ++i ))
+for (( i=1; i <= viewers; ++i ))
 do
-  COMMAND="ffmpeg -i \"$m3u8_url\" -codec copy -f null /dev/null"
-  $COMMAND >>/dev/null 2>&1 &
+  COMMAND="ffmpeg -i "$m3u8_url" -codec copy -f null /dev/null"
+  $COMMAND &>>/dev/null &
   echo "running command $COMMAND"
 done
