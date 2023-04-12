fc9467fa11d7508cf62f40777d3ab73c7797d319
Mohit-3196 <86982446+Mohit-3196@users.noreply.github.com>
2023-04-12 20:59:57 +0530

    Update hls_players.sh
    Updating the script to remove error

--- hls_players.sh
+++ hls_players.sh
@@ -3,7 +3,7 @@
 m3u8_url=$1
 viewers=$2
 
-for (( i=1; i <= viewers; ++i ))
+for (( i=1; i <= $viewers; ++i ))
 do
   COMMAND="ffmpeg -i "$m3u8_url" -codec copy -f null /dev/null"
   $COMMAND &>>/dev/null &
