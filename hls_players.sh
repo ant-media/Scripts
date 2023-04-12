33fac6e2c77ea6e9b3d9fc53a1f65f5b3505b166
Mohit-3196 <86982446+Mohit-3196@users.noreply.github.com>
2023-04-12 15:50:10 +0530

    Create HLS viewers.
    The script will simulate a specified number of HLS viewers accessing a live stream.

---
+++ hls_players.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+m3u8_url=$1
+viewers=$2
+
+for (( i=1; i <= $viewers; ++i ))
+do
+  COMMAND="ffmpeg -i "$m3u8_url" -codec copy -f null /dev/null"
+  $COMMAND &>>/dev/null &
+  echo "running command $COMMAND"
+done
