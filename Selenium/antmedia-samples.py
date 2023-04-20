55a71f43cfa29477c8b2f9a09685c64f1f2e7cdd
yashtandon113 <82374739+yashtandon113@users.noreply.github.com>
2023-04-20 18:55:57 +0530

    Update antmedia-samples.py

--- Selenium/antmedia-samples.py
+++ Selenium/antmedia-samples.py
@@ -8,8 +8,6 @@ import urllib.parse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 # Function to send notification to Slack
