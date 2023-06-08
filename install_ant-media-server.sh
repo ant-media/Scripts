7c5c2d449bc798214f95285b031dd5990fb74fe7
Murat Ugur Eminoglu <murat@murat.ws>
2023-06-08 08:16:56 +0300

    Add busybox package for Ubuntu 22.04

--- install_ant-media-server.sh
+++ install_ant-media-server.sh
@@ -286,7 +286,7 @@ REQUIRED_VERSION="2.6"
 
 if [ "$ID" == "ubuntu" ]; then
   $SUDO apt-get update -y
-  $SUDO apt-get install unzip zip libva-drm2 libva-x11-2 libvdpau-dev -y
+  $SUDO apt-get install unzip zip libva-drm2 libva-x11-2 libvdpau-dev busybox -y
   VERSION=$(unzip -p "$ANT_MEDIA_SERVER_ZIP_FILE" ant-media-server/ant-media-server.jar  | busybox unzip -p - | grep -a "Implementation-Version"|cut -d' ' -f2 | tr -d '\r')
   
   # If the version is lower than 2.6 and the architecture is x86_64, install the libcrystalhd-dev package. 
