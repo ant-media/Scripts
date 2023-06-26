33b55d3bcb3d7c34d1e30b97b14574521ac2ef7d
Murat Ugur Eminoglu <murat@murat.ws>
2023-06-26 10:12:53 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -9,3 +9,21 @@ curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg --dearmor --yes --o
 echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+
+# Generate a random username and password
+username=$(openssl rand -hex 6)
+password=$(openssl rand -hex 12)
+
+# Start MongoDB and configure authentication
+sudo systemctl restart mongod
+
+sleep 10
+
+echo "use admin;
+db.createUser({ user: '$username', pwd: '$password', roles: ['root'], mechanisms: ['SCRAM-SHA-1'] });" | mongosh
+
+sudo sed -i 's/#security:/security:\n  authorization: "enabled"/g' /etc/mongod.conf
+sudo systemctl restart mongod
+
+echo "MongoDB username: $username"
+echo "MongoDB password: $password"
