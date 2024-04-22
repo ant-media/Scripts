16abb60891c20487a791127432edccf63127dd2e
burak-58 <39169933+burak-58@users.noreply.github.com>
2024-04-22 08:44:58 +0300

    edit init script

--- init.sh
+++ init.sh
@@ -46,8 +46,4 @@ then
 
   fi
   touch $INITIALIZED
-  ## Add default ServerSecretKey
-  SECRET_KEY=$(openssl rand -base64 32 | head -c 32)
-  sudo sed -i "/^server.jwtServerControlEnabled=/s|.*|server.jwtServerControlEnabled=true|" /usr/local/antmedia/conf/red5.properties
-  sudo sed -i "/^server.jwtServerSecretKey=/s|.*|server.jwtServerSecretKey=$SECRET_KEY|" /usr/local/antmedia/conf/red5.properties
 fi
