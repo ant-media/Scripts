ab31e22ec639805a6575ba27726832896d64e33c
Murat Ugur Eminoglu <murat@murat.ws>
2025-01-23 08:44:01 +0300

    Add support for multi-version MongoDB installation on Ubuntu

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -19,8 +19,23 @@ fi
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get install -y curl gnupg2
-curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/mongodb-server-6.0.gpg
-echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+ubuntu_version=$(lsb_release -rs)
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
+
+
+if [[ "$ubuntu_version" == "24.04" ]]; then
+  echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+elif [[ "$ubuntu_version" == "22.04" ]]; then
+  echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+elif [[ "$ubuntu_version" == "20.04" ]]; then
+  echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+else
+  echo "Unsupported Ubuntu version: $ubuntu_version"
+  exit 1
+fi
+
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 
