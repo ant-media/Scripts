0358a7df42076e78df1d1ee64546596e5eff9eab
Murat Ugur Eminoglu <murat@murat.ws>
2025-04-17 10:38:06 +0300

    Fix codename

--- monitor/install-graylog.sh
+++ monitor/install-graylog.sh
@@ -88,7 +88,7 @@ check_ip
 # MongoDB 
 curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
    sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/mongodb-server-8.0.gpg
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu `lsb_release -c -s`/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl enable mongod
