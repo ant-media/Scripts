7a6dca425d8af58db601d62fd74f87eec5817321
yashtandon113 <82374739+yashtandon113@users.noreply.github.com>
2026-04-27 19:03:16 +0530

    Update start_rtsp_server.sh

--- load-testing/rtsp_loadtest/start_rtsp_server.sh
+++ load-testing/rtsp_loadtest/start_rtsp_server.sh
@@ -1,9 +1,30 @@
-#!/bin/bash 
+#!/bin/bash
+
 BASEDIR=$(dirname "$0")
-cd $BASEDIR
-  
+cd "$BASEDIR"
 
-#./test-launch "( videotestsrc pattern=snow ! video/x-raw,width=1920,height=1080,format=I420 ! videoconvert ! x264enc ! rtph264pay name=pay0 pt=96 audiotestsrc ! audio/x-raw,channels=2,rate=48000 !  audioconvert ! avenc_aac ! rtpmp4apay name=pay1 pt=97 )"
+# Take MP4 file as argument
+MP4_FILE="$1"
 
+if [ -z "$MP4_FILE" ]; then
+    echo "Usage: $0 <mp4-file-path>"
+    exit 1
+fi
 
-./test-launch "( videotestsrc pattern=snow ! video/x-raw,width=1920,height=1080,format=I420,framerate=25/1 ! videoconvert ! x264enc bitrate=2000 tune=zerolatency ! rtph264pay name=pay0 pt=96 audiotestsrc ! audio/x-raw,channels=2,rate=48000 ! audioconvert ! avenc_aac ! rtpmp4apay name=pay1 pt=97 )"
+# Convert to absolute path (important for uri=file://)
+MP4_FILE_ABS=$(realpath "$MP4_FILE")
+
+echo "Using MP4 file: $MP4_FILE_ABS"
+
+./test-launch "( 
+uridecodebin uri=file://$MP4_FILE_ABS name=dec 
+
+dec. ! queue ! videoconvert ! videoscale ! videorate ! 
+video/x-raw,width=1920,height=1080,framerate=30/1 ! 
+x264enc bitrate=2000 key-int-max=60 tune=zerolatency speed-preset=veryfast ! 
+rtph264pay name=pay0 pt=96 
+
+dec. ! queue ! audioconvert ! audioresample ! 
+audio/x-raw,rate=48000,channels=2 ! 
+avenc_aac ! rtpmp4apay name=pay1 pt=97 
+)"
